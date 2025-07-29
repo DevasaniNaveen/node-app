a4d512ffcd878c2eaa97044cdfab2cf6753cd21c
DevasaniNaveen <devasaninawin@gmail.com>
2025-07-29 16:25:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from your CI/CD pipeline! Team');
+  res.send('🚀 Hello from your CI/CD pipeline!');
 });
 
 app.listen(port, () => {
